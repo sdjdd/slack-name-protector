3d75cacda8a79eff9db9c14249039af841d0fe79
sdjdd <i@sdjdd.com>
2022-10-14 12:58:45 +0800

    feat: switch to passive mode

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const http = require('node:http');
  * @type {import('axios').default}
  */
 const axios = require('axios');
+const parse = require('parse-duration').default;
 
-const { SLACK_API_BASE_URL, SLACK_COOKIE, SLACK_TOKEN } = process.env;
+const {
+  SLACK_API_BASE_URL = 'https://slack.com/api',
+  SLACK_COOKIE,
+  SLACK_TOKEN,
+  SLACK_DISPLAY_NAME,
+  CHECK_INTERVAL = '1min',
+} = process.env;
 
 const client = axios.create({
   baseURL: SLACK_API_BASE_URL,
@@ -16,24 +23,37 @@ const client = axios.create({
   proxy: false,
 });
 
-async function resetDisplayName() {
-  const res = await client.post('/users.profile.set', {
-    profile: {
-      display_name: '',
-    },
-  });
+async function getProfile() {
+  const res = await client.post('/users.profile.get');
+  if (!res.data.ok) {
+    throw new Error(res.data.error);
+  }
+  return res.data.profile;
+}
+
+async function setProfile(profile) {
+  const res = await client.post('/users.profile.set', { profile });
   if (!res.data.ok) {
     throw new Error(res.data.error);
   }
 }
 
+async function resetDisplayName() {
+  const profile = await getProfile();
+  if (profile.display_name !== SLACK_DISPLAY_NAME) {
+    await setProfile({
+      display_name: SLACK_DISPLAY_NAME,
+    });
+    console.log('display_name has been reset');
+  } else {
+    console.log('display_name not changed');
+  }
+}
+
+resetDisplayName().catch(console.error);
 setInterval(() => {
-  resetDisplayName()
-    .then(() => {
-      console.log('display_name updated');
-    })
-    .catch(console.error);
-}, 1000 * 60);
+  resetDisplayName().catch(console.error);
+}, parse(CHECK_INTERVAL, 'millisecond'));
 
 http
   .createServer((req, res) => {
